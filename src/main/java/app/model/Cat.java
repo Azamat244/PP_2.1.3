d78adb0d1eeb752e30585327fc50e59966a68b9f
Azamat244 <152506960+Azamat244@users.noreply.github.com>
2024-12-23 17:09:59 +0500

    решение задачи 2.1.3

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -2,8 +2,9 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component ("catBean")
 public class Cat extends Animal {
+
     @Override
     public String toString() {
         return "Im a Cat";
